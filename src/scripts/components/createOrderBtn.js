2acf9f461a4126076a96811dbd25bd30382abd88
dimitricbrown <dimitricbrown@outlook.com>
2023-03-14 19:54:01 -0500

    FEAT: Added Create an Order button to the welcome page

---
+++ src/scripts/components/createOrderBtn.js
@@ -0,0 +1,5 @@
+//  import renderToDOM
+
+//  const createOrderBtn = '<button class="btn btn-info btn-lg mb-4" id="create-order-btn">Create an Order</button>';
+
+//  renderToDOM('#add-button', createOrderBtn);
