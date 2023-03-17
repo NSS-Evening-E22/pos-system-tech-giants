f6fa90bc1a90bac4c4865497f9f31bf5fac0e727
bhighlander <saphrimangel@gmail.com>
2023-03-16 20:45:47 -0500

    REFACTOR: refactored buttons on viewOrders.js

--- src/scripts/helpers/clearDom.js
+++ src/scripts/helpers/clearDom.js
@@ -1,8 +1,5 @@
 const clearDom = () => {
-  document.querySelector('#cards').innerHTML = '';
-  document.querySelector('#filter-buttons').innerHTML = '';
-  document.querySelector('#form-container').innerHTML = '';
-  document.querySelector('#cards').innerHTML = '';
+
 };
 
 export default clearDom;
