ab7bd46fa6f021503dd03b1f67839c340286b981
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-03-16 21:53:36 -0500

    Added forms and order types

---
+++ src/scripts/components/forms/selectOrderTypes.js
@@ -0,0 +1,25 @@
+
+import renderToDom from '../../helpers/renderToDom';
+
+const selectOrdTypes = (typeId) => {
+  let domString = `<label for="author">Select an Author</label>
+    <select class="form-control" id="author_id" required>
+    <option value="">Select an Author</option>`;
+
+  getOrderTypes().then((ordTypesArray) => {
+    ordTypesArray.forEach((types) => {
+      domString += `
+          <option 
+            value="${types.firebaseKey}" 
+            ${typeId === types.firebaseKey ? 'selected' : ''}>
+              ${types.first_name} ${types.last_name}
+          </option>`;
+    });
+
+    domString += '</select>';
+
+    renderToDom('#select-ordertypes', domString);
+  });
+};
+
+export default selectOrdTypes;
