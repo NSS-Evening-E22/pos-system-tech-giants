0d392fbc2fbf1abee15bbe5f9be1c78021969a69
bhighlander <saphrimangel@gmail.com>
2023-03-18 12:24:06 -0500

    FIX: fixed navbar links

--- src/scripts/helpers/clearDom.js
+++ src/scripts/helpers/clearDom.js
@@ -2,10 +2,9 @@ const clearDom = () => {
   document.querySelector('#welcome').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
-  // document.querySelector('#view').innerHTML = '';
+  document.querySelector('#order-container').innerHTML = '';
   // document.querySelector('#cards').innerHTML = '';
   // document.querySelector('#filter-buttons').innerHTML = '';
-
 };
 
 export default clearDom;
