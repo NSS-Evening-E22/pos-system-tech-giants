6d735906a5e83b7434750f012a15b4378140d163
dimitricbrown <dimitricbrown@outlook.com>
2023-03-18 11:15:11 -0500

    Rendered the buttons/welcome and cleaned up the clearDom.js

--- src/scripts/components/createOrderBtn.js
+++
@@ -1,5 +0,0 @@
-import renderToDom from '../helpers/renderToDom';
-
-const createOrderBtn = '<button class="btn btn-info btn-lg mb-4" id="create-order-btn">Create an Order</button>';
-
-renderToDom('#add-button', createOrderBtn);
--- src/scripts/components/shared/pages/homeLoggedIn.js
+++ src/scripts/components/shared/pages/homeLoggedIn.js
@@ -1,13 +1,18 @@
+import clearDom from '../../../helpers/clearDom';
 import renderToDom from '../../../helpers/renderToDom';
-import createOrderBtn from '../../createOrderBtn';
 
-const homeLoggedIn = (user) => {
-  // clearDom();
-  const domString = `<h1>Welcome ${user.name}</h1>
-    <button>View Orders</button>
-    ${createOrderBtn()}
-    <button>View Revenue</button>`;
-  renderToDom('#app', domString);
+const homeLoggedIn = () => {
+  clearDom();
+
+  const domString = '<h1>Welcome, USER </h1>';
+  renderToDom('#welcome', domString);
+
+  const btnString = `
+    <button class="btn btn-success btn-lg mb-4" id="view-orders">View Orders</button>
+    <button class="btn btn-info btn-lg mb-4" id="create-order">Create an Order</button>
+    <button class="btn btn-warning btn-lg mb-4" id="view-revenue">View Revenue</button>
+  `;
+  renderToDom('#add-button', btnString);
 };
 
 export default homeLoggedIn;
