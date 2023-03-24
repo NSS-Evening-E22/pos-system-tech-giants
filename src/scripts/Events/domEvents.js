f0de838e92851e57f1c5d3af21b2bbf982c06627
dimitricbrown <dimitricbrown@outlook.com>
2023-03-23 20:38:52 -0500

    FIX: Welcome page buttons are working and revenue page renders..still fixing the NAN issue

--- src/scripts/Events/domEvents.js
+++ src/scripts/Events/domEvents.js
@@ -1,5 +1,25 @@
-const domEvents = () => {
+import { getOrders } from '../../api/orderData';
+import addEditorderForm from '../components/forms/orderForm';
+import viewRevenue from '../components/shared/pages/revenue';
+import { viewAllOrders } from '../components/shared/pages/viewOrders';
 
+const domEvents = () => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    // TODO: ADD CLICK EVENT FOR VIEWING ORDERS
+    if (e.target.id.includes('view-orders-btn')) {
+      getOrders().then(viewAllOrders);
+    }
+
+    // TODO: ADD CLICK EVENT FOR CREATING ORDERS
+    if (e.target.id.includes('create-order-btn')) {
+      addEditorderForm();
+    }
+
+    // TODO: ADD CLICK EVENT FOR VIEW REVENUE
+    if (e.target.id.includes('view-revenue-btn')) {
+      getOrders().then(viewRevenue);
+    }
+  });
 };
 
 export default domEvents;
